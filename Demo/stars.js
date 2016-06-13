65ff1abf9e14202ead1be7300e51cb2bc29614af
bartlettmic <bartlettmic@gmail.com>
2016-06-13 17:49:20 -0600

    Triangle test

--- Demo/stars.js
+++ Demo/stars.js
@@ -1,28 +1,15 @@
-  /*		#TO-DO#
-  • Gravity
-  • Dropdown for different rendering
-    ► round or square ends
-    ► circles
-    ► triangles
-    ► ????
-  • Text feedback for sliders – hovering JS text?
-  • Click to add dot (increase max value)
-  • Fade triangles better
-  • Make sure checkbox speedbug is fixed on merge
-  */
-  
-var mousePos = { x: window.innerWidth / 2, y: window.innerHeight / 2 },
+    mousePos = { x: window.innerWidth / 2, y: window.innerHeight / 2 },
     canvas = document.createElement('canvas'),
     context = canvas.getContext('2d'),
     dots = [],
     Lines = new Set(),
-    FPS = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ? 30 : 60,
-    stars = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ? 25 : 50,
-    maxDiv = -11.5,
-    maxDist = -1*Math.sqrt(Math.pow(window.innerWidth, 2) + Math.pow(window.innerHeight, 2)) / maxDiv,
+    FPS = 20,
+    stars = 3,   
+    maxDiv = 11.5,
+    maxDist = Math.sqrt(Math.pow(window.innerWidth, 2) + Math.pow(window.innerHeight, 2)) / maxDiv,
+    maxDist = 1600,
     speed = 0.25,
-    thick = 3.5,
-    //lines = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ? 3 : 5,
+    thick = 10,
     lines = 10,
     G = 200,
     gravity = false,
@@ -34,51 +21,32 @@ $(document).ready(function() {
     document.getElementById('canvas').appendChild(canvas);
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    $('input[type="range"]').each(function(i) {
-        this.value = window[this.id];
-    });
-    $('input[type="checkbox"]').each(function(i) {
-        this.checked = window[this.id] ? window[this.id].toString() : "";
-    });
     for (var i = 0; i < stars; i++) dots.push(new Dot(i));
     setInterval(loop, 1000 / FPS);
-    console.log(new Date().getTime());
+    dots[0].r=255; dots[0].g=127; dots[0].b=127;
+    dots[1].r=127; dots[1].g=255; dots[1].b=127;
+    dots[2].r=127; dots[2].g=127; dots[2].b=255;
+    //context.globalCompositeOperation = 'screen';
 });
 
 //update mouse position
 $(document).mousemove(function(e) {
     e.preventDefault();
-    mousePos = { x: e.clientX, y: e.clientY };
+    mousePos = { x: e.clientX, y: e.clientY	};
 });
 
-//Slider and checkbox updates
-$(function() {
-    $('input[type="range"]').change(function(e) {
-        if (e.target.id == 'speed') {
-            for (let d of dots) {
-                d.vel.x *= e.target.value / speed;
-                d.vel.y *= e.target.value / speed;
-            }
-        }
-
-        window[e.target.id] = e.target.value;
-        console.log(e.target.id + " -> " + e.target.value);
-
-        if (e.target.id == 'maxDiv') maxDist = -1*Math.sqrt(Math.pow(window.innerWidth, 2) + Math.pow(window.innerHeight, 2)) / maxDiv;
-
-        if (stars < dots.length) {
-            dots = dots.slice(0, stars);
-        } else if (stars > dots.length)
-            for (var i = dots.length; i < stars; i++) dots.push(new Dot(i));
-    });
-
-    $('input[type="checkbox"]').change(function(e) {
-        window[e.target.value] = e.target.checked;
-        if (tether) for (let d of dots) { d.vel.x *= 2; d.vel.y *= 2; }
-        else for (let d of dots) { d.vel.x /= 2;  d.vel.y /= 2; }
-    });
-})
-
+//Mouse click
+$(document).click(function(e) {
+	var closest = window.innerWidth + window.innerHeight, index = 0;
+	for (var i = 0; i < dots.length; i++) {		
+		var distance = Math.sqrt(Math.pow(dots[i].pos.x - mousePos.x, 2) + Math.pow(dots[i].pos.y - mousePos.y, 2));
+		if (distance < closest) {
+			closest = distance; index = i;
+    }
+  }
+  dots[index].pos.x = mousePos.x;
+  dots[index].pos.y = mousePos.y;
+});
 
 //Loop function
 function loop() {
@@ -86,22 +54,21 @@ function loop() {
     if (window.innerWidth != canvas.width || window.innerHeight != canvas.height) {
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
-        maxDist = -1*Math.sqrt(Math.pow(window.innerWidth, 2) + Math.pow(window.innerHeight, 2)) / maxDiv;
+        maxDist = Math.sqrt(Math.pow(window.innerWidth, 2) + Math.pow(window.innerHeight, 2)) / maxDiv;
     }
+
     context.clearRect(0, 0, canvas.width, canvas.height);
-    for (var i = 0; i < dots.length; i++) dots[i].update();
+
     for (var i = 0; i < dots.length; i++) dots[i].friend();
     Lines.clear();
     for (var i = 0; i < dots.length; i++) dots[i].lines();
     Render(context);
 }
 
-
 //Dot class constructor
 function Dot(ID) {
     this.pos = { x: Math.random() * window.innerWidth, y: Math.random() * window.innerHeight };
     this.vel = { x: Math.random() * speed * (Math.round(Math.random()) ? 1 : -1), y: 0 };
-    //this.vel.y = Math.random() * speed * (Math.round(Math.random()) ? 1 : -1);
     this.vel.y = Math.sqrt(Math.pow(speed, 2) - Math.pow(this.vel.x, 2)) * (Math.round(Math.random()) ? 1 : -1)
     this.r = Math.round(Math.random() * 255);
     this.g = Math.round(Math.random() * 255);
@@ -110,31 +77,12 @@ function Dot(ID) {
     this.ids = new Map();
 }
 
-//Update Dot's position
-Dot.prototype.update = function() {
-    if (tether && this.ids.size > 0) {
-        X = this.vel.x, Y = this.vel.y;
-        for (let d of this.ids) {
-            if (!dots[d]) break;
-            X += dots[d].vel.x;
-            Y += dots[d].vel.y;
-        }
-        X /= (this.ids.size + 1);
-        Y /= (this.ids.size + 1);
-        this.pos.x += (4 * X + this.vel.x) / 5;
-        this.pos.y += (4 * Y + this.vel.y) / 5;
-    } 
-    else { this.pos.x += this.vel.x; this.pos.y += this.vel.y; }
-    
-    if (this.pos.x <= 0) this.vel.x *= (this.vel.x > 0 ? 1 : -1);
-    if (this.pos.x >= window.innerWidth) { this.vel.x *= (this.vel.x < 0 ? 1 : -1); this.pos.x = window.innerWidth; }
-    if (this.pos.y <= 0) this.vel.y *= (this.vel.y > 0 ? 1 : -1);
-    if (this.pos.y >= window.innerHeight) { this.vel.y *= (this.vel.y < 0 ? 1 : -1);  this.pos.y = window.innerHeight; }
-};
-
-//Check proximity of other Dots
 Dot.prototype.friend = function() {
-    this.ids.clear();
+    this.ids.clear();    
+    context.beginPath();
+    context.arc(this.pos.x, this.pos.y, 3, 0, 2 * Math.PI);
+    context.fillStyle = "rgba(255,255,255,1)";
+    context.fill();
     for (var i = 0; i < dots.length; i++) {
         if (lines > 0 && this.ids.size >= lines) break;
         if (this.id == i || dots[i].ids.has(this.id)) continue;
@@ -162,6 +110,7 @@ Dot.prototype.lines = function() {
     }
 };
 
+
 function Render(c) {
     for (let L of Lines) {
         var A = L.d1,
@@ -169,7 +118,9 @@ function Render(c) {
             maxArea = Math.pow(maxDist, 2) * Math.sqrt(3) / 4,
             maxPtoC = maxDist * Math.sqrt(3) / 3;
 
+        console.log("fuck1");
         for (var i of L.d1.ids.keys()) {
+            console.log("fuck2");
             if (dots[i].id == B.id || dots[i].id == A.id) continue;
             var C = dots[i],
                 center = { x: (A.pos.x + B.pos.x + C.pos.x) / 3, y: (A.pos.y + B.pos.y + C.pos.y) / 3 },
@@ -205,13 +156,13 @@ function Render(c) {
                 //cB = "rgba(" + B.r + "," + B.g + "," + B.b + "," + (1 * (1 - (area / maxArea)))/3 + ")",
                 //cC = "rgba(" + C.r + "," + C.g + "," + C.b + "," + (1 * (1 - (area / maxArea)))/3 + ")",
 
-                //cA = "rgba(" + A.r + "," + A.g + "," + A.b + ",1)",
-                //cB = "rgba(" + B.r + "," + B.g + "," + B.b + ",1)",
-                //cC = "rgba(" + C.r + "," + C.g + "," + C.b + ",1)",
+                cA = "rgba(" + A.r + "," + A.g + "," + A.b + ",1)",
+                cB = "rgba(" + B.r + "," + B.g + "," + B.b + ",1)",
+                cC = "rgba(" + C.r + "," + C.g + "," + C.b + ",1)",
 
-                cA = "rgba(" + A.r + "," + A.g + "," + A.b + "," + ((1 - (dA / maxDist))) + ")",
-                cB = "rgba(" + B.r + "," + B.g + "," + B.b + "," + ((1 - (dB / maxDist))) + ")",
-                cC = "rgba(" + C.r + "," + C.g + "," + C.b + "," + ((1 - (dC / maxDist))) + ")",
+                //cA = "rgba(" + A.r + "," + A.g + "," + A.b + "," + ((1 - (dA / maxDist))) + ")",
+                //cB = "rgba(" + B.r + "," + B.g + "," + B.b + "," + ((1 - (dB / maxDist))) + ")",
+                //cC = "rgba(" + C.r + "," + C.g + "," + C.b + "," + ((1 - (dC / maxDist))) + ")",
 
                 c0 = "rgba(0,0,0,0)";
 
@@ -220,7 +171,7 @@ function Render(c) {
                 c.arc(A.pos.x, A.pos.y, 3, 0, 2 * Math.PI);
                 c.arc(B.pos.x, B.pos.y, 3, 0, 2 * Math.PI);
                 c.arc(C.pos.x, C.pos.y, 3, 0, 2 * Math.PI);
-                c.fillStyle = "rgba(127,127,127,1)";
+                c.fillStyle = "rgba(255,255,255,1)";
                 c.fill();
                 */
 
@@ -234,12 +185,12 @@ function Render(c) {
             c.beginPath();
             c.moveTo(A.pos.x, A.pos.y);
 
-            //c.lineTo(B.pos.x, B.pos.y);
-            //c.lineTo(C.pos.x, C.pos.y);
+            c.lineTo(B.pos.x, B.pos.y);
+            c.lineTo(C.pos.x, C.pos.y);
 
-            c.quadraticCurveTo(center.x, center.y, B.pos.x, B.pos.y);
-            c.quadraticCurveTo(center.x, center.y, C.pos.x, C.pos.y);
-            c.quadraticCurveTo(center.x, center.y, A.pos.x, A.pos.y);
+            //c.quadraticCurveTo(center.x, center.y, B.pos.x, B.pos.y);
+            //c.quadraticCurveTo(center.x, center.y, C.pos.x, C.pos.y);
+            //c.quadraticCurveTo(center.x, center.y, A.pos.x, A.pos.y);
 
             c.fillStyle = gA;
             c.fill();
@@ -250,11 +201,7 @@ function Render(c) {
             c.fillStyle = gC;
             c.fill();
         }
-
-
-
     }
 };
 
-//Line class constructor
 function Line(dot1, dot2) { this.d1 = dot1; this.d2 = dot2; }
